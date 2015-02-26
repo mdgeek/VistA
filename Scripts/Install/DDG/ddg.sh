073ba6b416d4cf807ecbbc8f711e0932af1cfa04
Douglas Martin <dkmartin@regenstrief.org>
2015-02-26 15:28:59 -0500

    Pull dEWDrop globals from GitHub.

--- Scripts/Install/DDG/ddg.sh
+++ Scripts/Install/DDG/ddg.sh
@@ -6,8 +6,8 @@ dos2unix /vagrant/DDG/*.RSA > /dev/null
 #
 # Replace globals
 #
-curl -L -s https://www.dropbox.com/s/yy8v3mo2xhnm4io/dewdrop.zwr.zip?dl=1 -o /tmp/dewdrop.zwr.zip
-unzip /tmp/dewdrop.zwr.zip -d /tmp
+curl -L -s http://www.carewebframework.org/downloads/dewdrop.zwr_ -o /tmp/dewdrop.zwr_
+unzip /tmp/dewdrop.zwr_ -d /tmp
 python /scripts/Scripts/VistARoutineImport.py ./RGZDDG.RSA -S 2 -o /home/osehra/r
 gtm -run DDG^RGZDDG /tmp/dewdrop.zwr
 rm /tmp/dewdrop.*
