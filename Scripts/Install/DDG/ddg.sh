c22a53fb75913c14cba401858f90e7f6d0e4378e
Douglas Martin <dkmartin@regenstrief.org>
2015-02-26 22:42:38 -0500

    Update encryption routine.

--- Scripts/Install/DDG/ddg.sh
+++ Scripts/Install/DDG/ddg.sh
@@ -9,6 +9,7 @@ dos2unix /vagrant/DDG/*.RSA > /dev/null
 curl -L -s http://www.carewebframework.org/downloads/dewdrop.zwr_ -o /tmp/dewdrop.zwr_
 unzip /tmp/dewdrop.zwr_ -d /tmp
 python /scripts/Scripts/VistARoutineImport.py ./RGZDDG.RSA -S 2 -o /home/osehra/r
+python /scripts/Scripts/VistARoutineImport.py ./XUSHSH.RSA -S 2 -o /home/osehra/r
 gtm -run DDG^RGZDDG /tmp/dewdrop.zwr
 rm /tmp/dewdrop.*
 
