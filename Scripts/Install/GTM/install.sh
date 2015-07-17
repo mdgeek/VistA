5007c673cfc1ac453d50a392476941e39f837ffc
Douglas Martin <dkmartin@regenstrief.org>
2015-07-17 08:54:30 -0400

    Updates.

--- Scripts/Install/GTM/install.sh
+++ Scripts/Install/GTM/install.sh
@@ -25,6 +25,17 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Download gtminstall script from SourceForge
+echo "Downloading gtminstall"
+curl -s --remote-name -L http://downloads.sourceforge.net/project/fis-gtm/GT.M%20Installer/v0.13/gtminstall
+
+# Verify hash as we are going to make it executable
+sha1sum -c --status gtminstall_SHA1
+if [ $? -gt 0 ]; then
+    echo "Something went wrong downloading gtminstall"
+    exit $?
+fi
+
 # Get kernel.shmmax to determine if we can use 32k strings
 shmmax=$(sysctl -n kernel.shmmax)
 
