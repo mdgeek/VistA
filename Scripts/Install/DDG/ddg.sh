b99f67c249c03f4c4080fbd481a083ee9c1b2bc0
Douglas Martin <dkmartin@regenstrief.org>
2015-08-06 11:57:22 -0400

    Updates.

--- Scripts/Install/DDG/ddg.sh
+++ Scripts/Install/DDG/ddg.sh
@@ -6,7 +6,7 @@ dos2unix ./*.rsa > /dev/null
 #
 # Replace globals
 #
-curl -L -s http://www.carewebframework.org/downloads/dewdrop.zwr.zip -o /tmp/dewdrop.zwr.zip
+curl http://carewebframework.github.io/downloads/dewdrop.zwr.zip -o /tmp/dewdrop.zwr.zip
 unzip /tmp/dewdrop.zwr.zip -d /tmp
 python ../../VistARoutineImport.py ./ddg.rsa -S 2 -o /home/osehra/r
 gtm -run DDG^RGZDDG /tmp/dewdrop.zwr
